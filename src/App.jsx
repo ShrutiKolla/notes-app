5d58ceb2449e27d096feb9a8d0125b52e7fd0104
me-mystic <Shrutikolla.2002@gmail.com>
2023-08-29 13:50:39 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Split from "react-split";
+import { nanoid } from "nanoid";
+import { useState } from 'react';
 
+// components
+import Editor from "./Components/Editor.jsx";
+import Sidebar from "./Components/Sidebar.jsx";
 function App() {
-
+  const [notes, setNotes] = useState([]);
   return (
-    <>
-      <p>hello</p>
-    </>
+    <main>
+      {notes.length === 0 ?
+        <Split
+          sizes={[70, 30]}
+>
+          <Sidebar />
+          <Editor />
+        </Split> : ""}
+    </main>
   )
 }
 
