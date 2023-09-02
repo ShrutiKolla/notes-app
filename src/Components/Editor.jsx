dea040356e60a18ef8785f479451dc52f4eef973
me-mystic <Shrutikolla.2002@gmail.com>
2023-09-02 13:16:19 +0530

    .

--- src/Components/Editor.jsx
+++ src/Components/Editor.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 // import ReactMde from "react-mde"
-import ReactMde from "react-mde";
-import 'react-mde/lib/styles/css/react-mde-all.css';
+// import ReactMde from "react-mde";
+// import 'react-mde/lib/styles/css/react-mde-all.css';
 import Showdown from "showdown"
 
 export default function Editor({ currNote, updateNote }) {
